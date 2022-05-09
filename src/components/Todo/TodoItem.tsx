ec280d2eae68d490a7bc59a2d1ca7f1003f57dc2
Eungwang <eungwang1203@gmail.com>
2022-05-09 19:54:05 +0900

    fetch : TodoItem Layout

---
+++ src/components/Todo/TodoItem.tsx
@@ -0,0 +1,28 @@
+import React, { useCallback } from "react";
+import { ISampleTodo } from "./db";
+export interface ITodoItem {
+  todo: ISampleTodo;
+  onToggle?: (id: number) => void;
+  onRemove?: (id: number) => void;
+}
+
+const TodoItem = ({ todo, onToggle, onRemove }: ITodoItem) => {
+  const { id, text, done } = todo;
+  const toggle = useCallback(() => onToggle && onToggle(id), [id, onToggle]);
+  const remove = useCallback(() => onRemove && onRemove(id), [id, onRemove]);
+  return (
+    <li>
+      <span
+        style={{
+          textDecoration: done ? "line-through" : "none",
+        }}
+        onClick={() => onToggle && onToggle(id)}
+      >
+        {text}
+      </span>
+      <button onClick={() => onRemove && onRemove(id)}>삭제</button>
+    </li>
+  );
+};
+
+export default TodoItem;
