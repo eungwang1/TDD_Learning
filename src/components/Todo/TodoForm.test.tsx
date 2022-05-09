d44c9e3c349e59aa4222c59543a8c08abdbeb1d6
Eungwang <eungwang1203@gmail.com>
2022-05-09 17:50:45 +0900

    test : TodoForm

---
+++ src/components/Todo/TodoForm.test.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import TodoForm from "./TodoForm";
+
+describe("<TodoForm />", () => {
+  const setup = (props = {}) => {
+    render(<TodoForm {...props} />);
+    const input = screen.getByPlaceholderText("할 일을 입력하세요"); // input 이 있는지 확인
+    const button = screen.getByText("등록"); // button이 있는지 확인
+    return {
+      input,
+      button,
+    };
+  };
+  it("has input and a button", () => {
+    const { input, button } = setup();
+    expect(input).toBeTruthy(); // 해당 값이 truthy 한 값인지 확인
+    expect(button).toBeTruthy();
+  });
+
+  it("changes input", () => {
+    const { input } = setup();
+    expect(input).toBeInTheDocument();
+    fireEvent.change(input, {
+      target: {
+        value: "TDD 배우기",
+      },
+    });
+    expect(input).toHaveAttribute("value", "TDD 배우기");
+  });
+
+  it("calls onInsert and clears input", () => {
+    const onInsert = jest.fn();
+    const { input, button } = setup({ onInsert });
+    // 수정하고
+    fireEvent.change(input, {
+      target: {
+        value: "TDD 배우기",
+      },
+    });
+    // 버튼 클릭
+    fireEvent.click(button);
+    expect(onInsert).toBeCalledWith("TDD 배우기"); // onInsert 가 'TDD 배우기' 파라미터가 호출됐어야함
+    expect(input).toHaveAttribute("value", ""); // input이 비워져야함
+  });
+});
